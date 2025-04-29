3a0b37e16910905159df9b35b650d410875ca0ad
Neha Budhiraja <35890155+nehab19@users.noreply.github.com>
2025-04-30 00:20:35 +0530

    Delete proxy directory

--- proxy/Employee.java
+++
@@ -1,6 +0,0 @@
-package guru.springframework.spring5webapp.designpattern.proxy;
-
-public interface Employee {
-    public void create(String client,Employee employee) throws Exception;
-    public void delete(String client,int employeeId) throws Exception;
-}
